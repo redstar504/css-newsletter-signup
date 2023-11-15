d7ab37bd411ab330d763676b7bebc5b8e2cb62fc
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2023-11-15 15:20:53 -0800

    Display fixes and show saved email

--- form.js
+++ form.js
@@ -1,19 +1,32 @@
+const savedEmail = sessionStorage.getItem('email');
+if (savedEmail) {
+    const savedEmailDisplay = document.getElementById('savedEmail');
+    console.log(savedEmailDisplay);
+    savedEmailDisplay.textContent = savedEmail;
+    sessionStorage.clear();
+}
+
 const form = document.getElementById('subscribe');
 const emailAddress = document.getElementById('emailAddress');
 
-form.addEventListener('submit', (e) => {
-    let isValid = true;
 
-    if (!emailAddress.value) {
-        isValid = false;
-    }
+if (form) {
+    form.addEventListener('submit', (e) => {
+        let isValid = true;
 
-    if (null === emailAddress.value.match(/.+@.+\..+/g)) {
-        isValid = false;
-    }
+        if (!emailAddress.value) {
+            isValid = false;
+        }
 
-    if (false === isValid) {
-        form.classList.add('invalid');
-        e.preventDefault();
-    }
-});
+        if (null === emailAddress.value.match(/.+@.+\..+/g)) {
+            isValid = false;
+        }
+
+        if (false === isValid) {
+            form.classList.add('invalid');
+            e.preventDefault();
+        }
+
+        sessionStorage.setItem("email", emailAddress.value);
+    });
+}
